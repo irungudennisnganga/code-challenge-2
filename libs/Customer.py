c52d95c76ce7a5fcd4f6ee0534a9093a5ffcf278
+dennisngangairungu <+>
2023-12-17 13:21:08 +0300

    added the files and code

--- libs/Customer.py
+++ libs/Customer.py
@@ -1,2 +1,30 @@
 class Customer:
-    pass
+    all_customers =[]
+    
+    def __init__(self, name, f_name):
+        self.name = name
+        self.f_name = f_name  
+        full_names= f"{name } {f_name}"
+        
+        self.all(full_names) 
+    
+    
+    def given_name(self):
+        return self.name
+            
+    def family_name(self):
+        return self.f_name   
+    
+    def full_name(self):
+        return (f"{self.name} {self.f_name}")     
+    
+    @classmethod
+    def all(cls, full_names):
+        return cls.all_customers.append(full_names)
+        #print([name for name in cls.all_customers])
+        
+    
+    
+dennis =Customer("dennis" , "irungu")   
+ 
+
--- libs/Restaurant.py
+++ libs/Restaurant.py
@@ -1,2 +1,16 @@
 class Restaurant:
-    pass
+    def __init__(self, name):
+        if type(name) == str :
+            self.name = name
+            #print(name)
+        else:
+            print("Enter valid data")    
+    
+    def name(self):
+        return self.name
+    
+
+dennis =Restaurant('Five Star')
+    
+    
+        
